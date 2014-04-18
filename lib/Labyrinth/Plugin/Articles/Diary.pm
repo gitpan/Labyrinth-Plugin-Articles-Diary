ccc3fbfb3533fe00d6484f8eae8dbbaec3e603b1
Barbie <barbie@missbarbell.co.uk>
2014-04-18 06:17:10 +0000

    Import of BARBIE/Labyrinth-Plugin-Articles-Diary-1.05 from CPAN.
    gitpan-cpan-distribution: Labyrinth-Plugin-Articles-Diary
    gitpan-cpan-version:      1.05
    gitpan-cpan-path:         BARBIE/Labyrinth-Plugin-Articles-Diary-1.05.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Labyrinth/Plugin/Articles/Diary.pm
+++ lib/Labyrinth/Plugin/Articles/Diary.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '1.04';
+$VERSION = '1.05';
 
 =head1 NAME
 
@@ -531,7 +531,7 @@ Miss Barbell Productions, L<http://www.missbarbell.co.uk/>
 
 =head1 COPYRIGHT & LICENSE
 
-  Copyright (C) 2002-2013 Barbie for Miss Barbell Productions
+  Copyright (C) 2002-2014 Barbie for Miss Barbell Productions
   All Rights Reserved.
 
   This module is free software; you can redistribute it and/or
